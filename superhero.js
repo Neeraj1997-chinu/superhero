1adb251fc0753de6527f9aadc7a11becfe796a76
Neeraj1997-chinu <58322212+Neeraj1997-chinu@users.noreply.github.com>
2020-11-30 17:38:48 +0530

    superheroes ajax added

---
+++ superhero.js
@@ -0,0 +1,49 @@
+// let url='https://superheroapi.com/api/access-token/character-id';
+
+//function load thhe list of the super hero name
+function load() {
+  for (let i = 1; i <= 731; i++) {
+    $.ajax({
+      //method
+      type: "GET",
+      // contentType: "application/json; charset=utf-8",
+      // dataType: "dataType",
+      //url of the superhero that gives json response back to us
+      url: "https://www.superheroapi.com/api.php/1230190087334410/" + i,
+
+      success: function (data) {
+        // console.log(data.name);
+        //creating and appending the  list attribute into the list
+        $(
+          `<li class='superhero' 
+                onclick='get_superhero(${i})' 
+                id='superhero_list${i}'><img src="${data.image.url}" style="height:40px; width:40px"> <p class="superherop">${data.name}</p></li>`,
+          {}
+        ).appendTo("#superhero_list");
+        $("<button></button>", {
+          text: "add to fav",
+          onclick: "Add_to_fav(" + i + ")",
+          class: "list_fav_button",
+        }).appendTo("#superhero_list" + i);
+      },
+    });
+  }
+}
+
+load();
+
+function search() {
+  var input = document.getElementById("search_superhero");
+  var flter = input.value.toUpperCase();
+  var superheroes = document.getElementsByClassName("superhero");
+  console.log(superheroes[0].childNodes[2].innerHTML);
+  var a;
+  for (var i = 0; i < superheroes.length; i++) {
+    a = superheroes[i].childNodes[2].innerHTML;
+    if (a.toUpperCase().indexOf(flter) > -1) {
+      superheroes[i].style.display = "";
+    } else {
+      superheroes[i].style.display = "none";
+    }
+  }
+}
